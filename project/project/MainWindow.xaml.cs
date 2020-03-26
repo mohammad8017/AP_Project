6840958c47a4719e17425234c61fc2c0af81f3a2
mohammad8017 <mhmd.ghfz@gmail.com>
2020-03-26 18:08:09 +0430

    first commit

---
+++ project/project/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Timer.Tick += new EventHandler(Timer_Click);
+
+            //Timer.Interval = new TimeSpan(0, 0, 1);
+
+            //Timer.Start();
+            DateTime d;
+
+            d = DateTime.Now;
+
+            label1.Content = d.Hour + " : " + d.Minute + " : " + d.Second;
+        }
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+        private void Btn3_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+        private void Btn4_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+       
+        private void Btn5_Click(object sender, RoutedEventArgs e)
+        {
+            tabControl.SelectedItem = tab2;
+        }
+
+        private void Btn6_Click(object sender, RoutedEventArgs e)
+        {
+            tabControl.SelectedItem = tab1;
+        }
+
+        private void Btn11_Click(object sender, RoutedEventArgs e)
+        {
+            System.Globalization.PersianCalendar p1 = new System.Globalization.PersianCalendar();
+            txt11.Text = p1.GetYear(DateTime.Now).ToString() + "/" + p1.GetMonth(DateTime.Now).ToString("0#") + "/" + p1.GetDayOfMonth(DateTime.Now).ToString("0#");
+        }
+
+        private void Btn22_Click(object sender, RoutedEventArgs e)
+        {
+            txt22.Text= DateTime.Now.ToLongTimeString();
+        }
+
+        private void Timer_Click(object sender, EventArgs e)
+
+        {
+
+            DateTime d;
+
+            d = DateTime.Now;
+
+            label1.Content = d.Hour + " : " + d.Minute + " : " + d.Second;
+
+        }
+    }
+}
